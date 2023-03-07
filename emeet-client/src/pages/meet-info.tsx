5162d4a75f782df44f0d3b5e49fac5528d3bee63
Phuriphat Wongkhachonloetmetha <6510110374@email.psu.ac.th>
2023-03-08 01:53:36 +0700

    Fix the display of components (user)

--- emeet-client/src/pages/meet-info.tsx
+++ emeet-client/src/pages/meet-info.tsx
@@ -48,9 +48,10 @@ function MeetInfoList() {
            </div>
            <div className="card-layout">
             <TextField sx={{ m: 2, minWidth: 120 }} label="Search" placeholder="Topic" variant="outlined" value={searchFilter} onChange={handleChangeSearchFilter} />
-            <h4>การประชุมที่ยังไม่ถึง</h4>
+            <h4 style={{fontFamily:'Kanit',fontWeight:600,marginBottom:10}}>การประชุมที่ยังไม่ถึง</h4>
             <hr></hr>
             {annList.filter((announcement) => !announcement.isMeetingEnd).length > 0 ?
+            <div className="ann-con">
               <Grid container sx={{ p: 2 }} spacing={{ xs: 2, md: 3 }} columns={{ xs: 2, sm: 8, md: 12, lg: 12, xl: 10 }}>
                 {annList.filter((announcement) => !announcement.isMeetingEnd).map((announcement, index) => (
                   <Grid item xs={2} sm={4} md={4} lg={3} xl={2} key={index}>
@@ -58,14 +59,16 @@ function MeetInfoList() {
                   </Grid>
               ))}
               </Grid>
+              </div>
               :
               <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', minHeight: 150 }} marginRight={20}>
                 <Typography variant="body2" color="text.secondary">ไม่พบรายการการประชุม</Typography>
               </Box>
               }
-              <h4>การประชุมที่จบไปเเล้ว</h4>
+              <h4 style={{fontFamily:'Kanit',fontWeight:600,marginBottom:10}}>การประชุมที่จบไปเเล้ว</h4>
               <hr></hr>
               {annList.filter((announcement) => announcement.isMeetingEnd).length > 0 ?
+              <div className="ann-con">
                 <Grid container sx={{ p: 2 }} spacing={{ xs: 2, md: 3 }} columns={{ xs: 2, sm: 8, md: 12, lg: 12, xl: 10 }}>
                   {annList.filter((announcement) => announcement.isMeetingEnd).map((announcement, index) => (
                 <Grid item xs={2} sm={4} md={4} lg={3} xl={2} key={index}>
@@ -73,6 +76,7 @@ function MeetInfoList() {
                 </Grid>
               ))}
               </Grid>
+              </div>
               :
               <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', minHeight: 150 }} marginRight={20}>
                 <Typography variant="body2" color="text.secondary">ไม่พบรายการการประชุม</Typography>
