75d7a994dbdac41cc36630c7756ae96d2d48b1b9
Phuriphat Wongkhachonloetmetha <6510110374@email.psu.ac.th>
2023-03-16 22:47:25 +0700

    Test login page

---
+++ emeet-client/src/pages/login.test.tsx
@@ -0,0 +1,46 @@
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import Login from './login';
+
+describe('', () => {
+  it('test first text', () => {
+    render(
+      <MemoryRouter>
+        <Login />
+      </MemoryRouter>
+    );
+    expect(screen.getByText('E-Meeting')).toBeInTheDocument();
+  });
+  it('test second text', () => {
+    render(
+      <MemoryRouter>
+        <Login />
+      </MemoryRouter>
+    );
+    expect(screen.getByText('รายการการประชุมสำหรับนักศึกษาคณะวิศวกรรมศาสตร์')).toBeInTheDocument();
+  });
+  it('test third text', () => {
+    render(
+      <MemoryRouter>
+        <Login />
+      </MemoryRouter>
+    );
+    expect(screen.getByText('อีเมล')).toBeInTheDocument();
+  });
+  it('test fourth text', () => {
+    render(
+      <MemoryRouter>
+        <Login />
+      </MemoryRouter>
+    );
+    expect(screen.getByText('รหัสผ่าน!')).toBeInTheDocument();
+  });
+  it('test button text', () => {
+    render(
+      <MemoryRouter>
+        <Login />
+      </MemoryRouter>
+    );
+    expect(screen.getByText('เข้าสู่ระบบ')).toBeInTheDocument();
+  });
+});
